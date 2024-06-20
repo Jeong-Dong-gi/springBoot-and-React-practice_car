57e6152402c25836b7d47e5681c1e0e3414e8819
Jeong-Dong-gi <159031134+Jeong-Dong-gi@users.noreply.github.com>
2024-06-20 16:07:10 +0900

    update

--- car/Carlist.js
+++ car/Carlist.js
@@ -1,33 +1,79 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { SERVER_URL } from "./constants"
-import {DataGrid} from "@mui/x-data-grid"
+// 데이터를 CSV로 내보내는 기능을 구현하기 위해 임포트
+import { DataGrid } from "@mui/x-data-grid"
+import { IconButton, Snackbar, Stack, Slide } from "@mui/material"
+import AddCar from "./AddCar"
+import EditCar from "./EditCar"
+import CustomToolbar from "./customToolbar"
+import {Delete} from "@mui/icons-material"
+
 
 // ag-grid 컴포넌트는 데이터 표의 열도 정의해야 한다.
 // 여기에는 field는 자동차 객체의 속성이다.
 // 열의 제목은 headerName속성으로 설정할 수 있다.
 // 열의 너비도 여기에서 설정할 수 있다.
-const columns=[
-  {field:"brand", headerName:"Brand", width:200},
-  {field:"model", headerName:"Model", width:200},
-  {field:"color", headerName:"Color", width:200},
-  {field:"year", headerName:"Year", width:150},
-  {field:"price", headerName:"Price", width:150},
-]
 
 function Carlist() {
   // REST API에서 가져온 자동차 정보를 담을 상태 객체가 필요하다.
   // 비어 있는 배열을 기본값으로 cars라는 상태를 선언한다.
   const [cars, setCars] = useState([])
+  // snackBar 컴포넌트의 open프롭 값은 부울이며, 이 값이 true면 컴포넌트가 표시된다.
+  // SnackBar 컴포넌트의 표시 여부를 처리하기 위해 open이라는 상태를 선언한다.
+  const[open, setOpen] = useState(false)
+
   // useEffect 훅에서 fetch를 실행한다.
   // fetch는 web server에 요청을 보내는 함수 - AJAX
   // 두 번째 인수로 비어 있는 배열을 전달하므로 fetch는 첫 번째 렌더링 후에 한번만 실행된다.
   // JSON응답 데이터에 있는 자동차 데이터는 cars상태에 저장한다.
 
+  const columns=[
+    {field:"brand", headerName:"Brand", width:200},
+    {field:"model", headerName:"Model", width:200},
+    {field:"color", headerName:"Color", width:200},
+    {field:"year", headerName:"Year", width:150},
+    {field:"price", headerName:"Price", width:150},
+    {
+      field:"edit",
+      headerName:"",
+      sortable:false,
+      filterable:false,
+      renderCell:(row)=>
+        <EditCar
+          data={row}
+          fetchCars={fetchCars}
+        />
+    },
+    {
+      field:"delete",
+      headerName:"",
+      sortable:false,
+      filterable:false,
+      // 셀에 더 복잡한 내용을 넣어야 할 때는 셀의 내용이 렌더링되는 방법을 정의하는 renderCell프롭으르 이용하면 된다.
+      // renderCell로 button 요소를 추가 한다.
+      renderCell:(row)=>
+        <IconButton onClick={()=>onDelClick(row.id)}>
+          <Delete color="warning" />
+        </IconButton>
+    },
+  ]
+
   const onDelClick=(url)=>{
-    fetch(url,{method:"delete"})
-      // 삭제 후에 새로운 목록으로 렌더링
-      .then(()=>fetchCars())
+    // delete 버튼을 눌렀을 때 확인 대화상자를 표시하는 기능이 있으면 자동차를 실수로 삭제하는 일이 없어 좋을 것이다.
+    // window 객체의 confirm메서드로 이 기능을 구현할 수 있다.
+    if(window.confirm("Are you sure to delete?")) {
+      fetch(url,{method:"delete"})
+        // 삭제 후에 새로운 목록으로 렌더링
+      .then((response)=>{
+        if(response.ok){
+          fetchCars()
+          setOpen(true)
+        } else {
+          alert("Something went wrong!")
+        }
+      })
       .catch(err=>console.error(err))
+    }
   }
   
   // 서버 주소 상수에서 목록을 가져오는 함수
@@ -41,20 +87,35 @@ function Carlist() {
         .catch(err=>console.error(err))
   }
 
-useEffect(()=>{
-    fetchCars()
-  })
+    useEffect(()=>{
+        fetchCars()
+      })
   /* DataGrid
     데이터 표의 데이터 원본은 fetch()로 읽어 들인 cars 상태이며, 이는 rows 프롭으로 정의된다.
     데이터 표 컴포넌트를 이용하려면 모든 행에 getRowId 프롭으로 고유한 ID속성을 정의해야 한다.
   */
-
+  
   return (
-    <div style={{height:500, width:"100%"}}>
+    <div>
+      <Stack m={2}>
+        <AddCar fetchCars={fetchCars} />
+      </Stack>
       <DataGrid
         columns={columns}
         rows={cars}
         getRowId={row=>row._links.self.href}
+        // 표에서 아무 행이나 클릭하면 그 행이 선택된다.
+        // 다음과 같이 표의 disableRowSelectionOnClick프롭을 true로 설정하면 이 동작을 비활성화할 수 있다.
+        disableRowSelectionOnClick={true}
+        components={{Toolbar:CustomToolbar}}
+      />
+      <Snackbar
+        // autoHideDuration 프롭은 onClose함수가 자동으로 호출되고 메시지가 사라지는 시간을 밀리초 단위로 정의한다.
+        // message프롭은 표시될 메시지를 정의한다.
+        open={open}
+        autoHideDuration={3000}
+        onClose={()=>setOpen(false)}
+        message="Car deleted"
       />
     </div>
   )
---
+++ car/customToolbar.js
@@ -0,0 +1,12 @@
+import { GridToolbarContainer, GridToolbarExport, gridClasses } from "@mui/x-data-grid"
+
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer
+      className={gridClasses.toolbarContainer}>
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  )
+}
+
+export default CustomToolbar
